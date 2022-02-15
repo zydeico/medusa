3e30c3a8e02302ebef1d6b23caed0731f7e4f5a7
Oliver Windall Juhl <59018053+olivermrbl@users.noreply.github.com>
2022-02-15 14:15:00 +0100

    fix: use /admin/returns/:id/receive for swap returns (#1041)

--- packages/medusa-react/test/hooks/admin/swaps/mutations.test.ts
+++ packages/medusa-react/test/hooks/admin/swaps/mutations.test.ts
@@ -1,8 +1,6 @@
 import { renderHook } from "@testing-library/react-hooks"
 import { fixtures } from "../../../../mocks/data"
 import {
-  useAdminCreateSwap,
-  useAdminFulfillSwap,
   useAdminCancelSwap,
   useAdminCancelSwapFulfillment,
   useAdminCreateSwap,
